e8e48476c0524435e1783f26560c28d5dfb1877e
XuanD <xuandeng1029@gmail.com>
2018-11-06 22:21:03 +0800

    xxx

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 import products.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.index, name="home"),
     path('accounts/', include('accounts.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
