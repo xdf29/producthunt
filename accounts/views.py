d4c9e2cf8025f78e3382b7cde270aca6865896d7
XuanD <xuandeng1029@gmail.com>
2018-11-06 22:08:05 +0800

    authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib.auth import authenticate, logout
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.get(username=request.POST["username"])
+                return render(request, "accounts/signup.html", {"error":"Username has been taken."})
+            except User.DoesNotExist:
+                user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+                auth.login(request, user)
+                return redirect("home")
+        else:
+            return render(request, "accounts/signup.html", {"error":"Password Mismatch."})
+    else:
+        return render(request, "accounts/signup.html")
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "accounts/login.html", {"error":"Invalid Username Or Password."})
+    else:
+        return render(request, "accounts/login.html")
+
+def logout(request):
+    if request.method == "POST":
+        auth.logout(request)
+        return redirect("login")
--- producthunt/urls.py
+++ producthunt/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import products.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.index, name="home"),
-    
+    path('accounts/', include('accounts.urls')),
 ]
