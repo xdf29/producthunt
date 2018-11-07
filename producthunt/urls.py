06267733c1b8190b58ef4bdffa80c7f2006f18c3
XuanD <xuandeng1029@gmail.com>
2018-11-07 17:59:29 +0800

    create product

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.index, name="home"),
     path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
+import datetime
 
 # Create your views here.
 def index(request):
     return render(request, "products/index.html")
+
+def create(request):
+
+    if request.method == "POST":
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['image'] and request.FILES['icon']:
+            product = Product()
+            product.title = request.POST['title']
+            product.pub_date = datetime.datetime.now()
+            product.body = request.POST['body']
+            if request.POST['url'].startswith("http://") or request.POST['url'].startswith("https://"):
+                product.url = request.POST['url']
+            else:
+                product.url = "http://" + request.POST['url']
+            product.image = request.FILES['image']
+            product.icon = request.FILES['icon']
+            product.hunter = request.user
+            product.save()
+            return redirect("home")
+        else:
+            return render(request, "products/create.html", {"error":"All Fields Are Required."})
+    else:
+        return render(request, "products/create.html")
